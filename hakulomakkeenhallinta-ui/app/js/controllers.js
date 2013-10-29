327c9cc24d1cbc938ef985e0f60de76e87a1ff6f
vpaju <ville.rajapaju@proactum.fi>
2013-10-29 15:22:33 +0200

    Added hakukohteen valinta

--- hakulomakkeenhallinta-ui/app/js/controllers.js
+++ hakulomakkeenhallinta-ui/app/js/controllers.js
@@ -5,7 +5,7 @@
 var controllers = angular.module('myApp.controllers', []);
 
 controllers.controller('HakulomakkeetCtrl', ['$scope', '$modal', '$log', function ($scope, $modal, $log) {
-    $scope.selections = {};
+    $scope.selections = [];
     $scope.applicationSystems = [
         {id: '1.1.1', name: 'Ensimmäinen haku'},
         {id: '1.1.2', name: 'Toinen haku'},
@@ -16,42 +16,52 @@ controllers.controller('HakulomakkeetCtrl', ['$scope', '$modal', '$log', functio
         alert('luoHakulomake()');
     };
     $scope.open = function () {
-        alert($scope.selections);
         var modalInstance = $modal.open({
-            templateUrl: 'modalTest.html',
-            controller: ModalInstanceCtrl,
-            resolve: {
-                items: function () {
-                    return $scope.applicationSystems;
-                }
-            }
+            templateUrl: 'partials/lisakysymykset/hakukohteen-valinta.html',
+            controller: ModalInstanceCtrl
         });
-
-        modalInstance.result.then(function (selectedItem) {
-            $scope.selected = selectedItem;
+        modalInstance.result.then(function (selections) {
+            $modal.open({
+                templateUrl: 'partials/lisakysymykset/kysymystyypin-valinta.html',
+                controller: ModalInstanceCtrl
+            });
         }, function () {
             $log.info('Modal dismissed at: ' + new Date());
         });
+    };
+    $scope.toggleCheck = function (item) {
+        if ($scope.selections.indexOf(item) === -1) {
+            $scope.selections.push(item);
+        } else {
+            $scope.selections.splice($scope.selections.indexOf(item), 1);
+        }
     };
 }]);
 
 controllers.controller('MallipohjatCtrl', [function () {
 }]);
 
-var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
-
-    $scope.items = items;
-    $scope.selected = {
-        item: $scope.items[0]
-    };
+var ModalInstanceCtrl = function ($scope, $modalInstance) {
+    $scope.selections = [];
+    $scope.items = [
+        {id: '1', name: 'Aasian tutkimus'},
+        {id: '2', name: 'Aasian tutkimus, kandidaatinopinnot'}
+    ];
 
     $scope.ok = function () {
-        $modalInstance.close($scope.selected.item);
+        $modalInstance.close($scope.selections);
     };
 
     $scope.cancel = function () {
         $modalInstance.dismiss('cancel');
     };
+    $scope.toggleCheck = function (item) {
+        if ($scope.selections.indexOf(item) === -1) {
+            $scope.selections.push(item);
+        } else {
+            $scope.selections.splice($scope.selections.indexOf(item), 1);
+        }
+    };
 };
 
-controllers.controller('ModalInstanceCtrl', ['$scope', '$modal', '$log', ModalInstanceCtrl]);
+//controllers.controller('ModalInstanceCtrl', ['$scope', '$modal', 'items', ModalInstanceCtrl]);
--- hakulomakkeenhallinta-ui/app/js/services.js
+++ hakulomakkeenhallinta-ui/app/js/services.js
@@ -5,5 +5,102 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
+var services = angular.module('myApp.services', []).
   value('version', '0.1');
+
+services.service('dialogService', ['$modal', '$log', '$rootScope', function ($modal, $log, $rootScope) {
+
+    var dialogDefaults = {
+        templateUrl: 'partials/common/dialog.html',
+        controller: "DialogServiceController",
+        backdrop: true, // true, "static", false
+        keyboard: true  // closable with ESC?
+    };
+
+    var dialogTextDefaults = {
+        title: "Really?",
+        description: "<i>Really</i> do <b>that</b>?",
+        ok: "YEAS please!",
+        cancel: "Naaaah..."
+    };
+
+    /**
+     * Returns ($modal) dialog instance which can then be used to wait for user interaction: dialogInstance.result.then(...
+     *
+     * customDialogTextDefaults:
+     * <ul>
+     *   <li>title: "foobar"</li>
+     *   <li>description: "foobar <b>text</b>"</li>
+     *   <li>ok: "Proceed"</li>
+     *   <li>cancel: "abort"</li>
+     * </ul>
+     *
+     * customDialogDefaults:
+     * <ul>
+     *   <li>backdrop: true, 'static', false</li>
+     *   <li>keyboard: true / false</li>
+     *   <li>templateUrl: 'MyController'</li>
+     *   <li>templateUrl: 'MyTemplate.html'</li>
+     * </ul>
+     *
+     * @param Object customDialogTextDefaults
+     * @param {type} customDialogDefaults
+     * @returns {@exp;$modal@call;open}
+     */
+    this.showDialog = function (customDialogTextDefaults, customDialogDefaults) {
+        var tempDialogDefaults = {};
+        angular.extend(tempDialogDefaults, dialogDefaults, customDialogDefaults);
+
+        var tempDialogTextDefaults = {};
+        angular.extend(tempDialogTextDefaults, dialogTextDefaults, customDialogTextDefaults);
+
+        if (!tempDialogDefaults.scope) {
+            tempDialogDefaults.scope = $rootScope.$new();
+        }
+        if (!tempDialogDefaults.scope.dialog) {
+            tempDialogDefaults.scope.dialog = tempDialogTextDefaults;
+        }
+
+        $log.info("showDialog(): ", tempDialogTextDefaults, tempDialogDefaults);
+
+        return $modal.open(tempDialogDefaults);
+    };
+
+
+    this.showNotImplementedDialog = function () {
+        var texts = {
+            title: "title",
+            description: "description",
+            ok: "ok",
+            cancel: "cancel"
+        };
+
+        return this.showDialog(texts);
+    };
+
+
+}]);
+
+/**
+ * Simple default controller for the "partials/common/dialog.html" template.
+ * Closes dialog with result of "ACTION" or "CANCEL".
+ *
+ * @param {type} param1
+ * @param {type} param2
+ */
+services.controller('DialogServiceController', ['$scope', '$log', '$modalInstance',
+    function ($scope, $log, $modalInstance) {
+
+        $log.info("dialogServiceController()");
+
+        $scope.onAction = function () {
+            $log.info("onAction()", $modalInstance);
+            $modalInstance.close("ACTION");
+        };
+
+        $scope.onClose = function () {
+            $log.info("onClose()", $modalInstance);
+            $modalInstance.close("CANCEL");
+        };
+
+    }]);
