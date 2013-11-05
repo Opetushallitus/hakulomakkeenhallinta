fbdfd0edf978f4726366d2881b692e777a0059e2
vpaju <ville.rajapaju@proactum.fi>
2013-11-05 15:27:10 +0200

    Update applicationForms after post

--- hakulomakkeenhallinta-ui/app/js/services.js
+++ hakulomakkeenhallinta-ui/app/js/services.js
@@ -8,9 +8,9 @@ services.service('Resources', ['$resource', function ($resource) {
         applicationSystems:         $resource('http://10.99.99.214:8080/hakulomakkeenhallinta-app/applicationSystems/:id'),
         applicationFormTemplates:   $resource('http://10.99.99.214:8080/hakulomakkeenhallinta-app/applicationFormTemplates/:id'),
         applicationForms:           $resource('http://10.99.99.214:8080/hakulomakkeenhallinta-app/applicationForms/:id'),
-        applicationOptions:         $resource('http://localhost:63342/hakulomakkeenhallinta/hakulomakkeenhallinta-ui/app/test-data/applicationOptions.json'),
-        additionalQuestions:        $resource('http://localhost:63342/hakulomakkeenhallinta/hakulomakkeenhallinta-ui/app/test-data/additionalQuestions.json'),
-        themes:                     $resource('http://localhost:63342/hakulomakkeenhallinta/hakulomakkeenhallinta-ui/app/test-data/themes.json'),
-        types:                      $resource('http://localhost:63342/hakulomakkeenhallinta/hakulomakkeenhallinta-ui/app/test-data/types.json')
+        applicationOptions:         $resource('http://localhost:8000/app/test-data/applicationOptions.json'),
+        additionalQuestions:        $resource('http://localhost:8000/app/test-data/additionalQuestions.json'),
+        themes:                     $resource('http://localhost:8000/app/test-data/themes.json'),
+        types:                      $resource('http://localhost:8000/app/test-data/types.json')
     };
 }]);
