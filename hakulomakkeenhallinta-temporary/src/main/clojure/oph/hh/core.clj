1d12c6b4a81977a06527be9194a00ffe9f71ec51
vpaju <ville.rajapaju@proactum.fi>
2014-04-15 15:19:44 +0300

    nimiavaruus muutettu

--- hakulomakkeenhallinta-temporary/src/main/clojure/oph/hh/core.clj
+++ hakulomakkeenhallinta-temporary/src/main/clojure/oph/hh/core.clj
@@ -2,11 +2,11 @@
   (:use compojure.core)
   (:use ring.adapter.jetty)
   (:require [compojure.route :as route]
-            [oph.api :as api]
+            [oph.hh.api :as api]
             [oph.hh.db :as db]
             [ring.middleware.params :refer [wrap-params]]))
 
 (defn boot []
-  (run-jetty #'api/all-routes {:port 8080 :join? false})
+  (run-jetty #'api/all-routes {:port 8080 :join? false}))
 
 
