b62cc4d0db4168ce196808cf254703dcdcda06d3
vpaju <ville.rajapaju@proactum.fi>
2014-05-05 10:40:43 +0300

    lisätty util-hakemisto

---
+++ hakulomakkeenhallinta-ui/app/scripts/services/util/formwalker.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('services.util', [])
+  .service('FormWalker', [ '_', function(_) {
+
+        var walker = _.walk(function(e) {
+            return e.children;
+        });
+
+        this.find = function(root, predicate) {
+            return walker.find(root, predicate);
+        };
+
+        this.filter = function(root, predicate) {
+            return walker.filter(root, _.walk.preorder, predicate);
+        };
+
+        this.filterByType = function(root, type) {
+            return this.filter(root, function(el) {
+                return el._class && el._class.indexOf(type) != -1;
+            });
+        };
+    }
+  ]);
