7316997d354998fcc5edf686ba110f5fb8c75d37
tomalatv <toni.latvakoski@gofore.com>
2014-05-15 11:55:44 +0300

    prosps conf serviceRootUri -> temporary

--- hakulomakkeenhallinta-ui/app/scripts/services/provider/props.js
+++ hakulomakkeenhallinta-ui/app/scripts/services/provider/props.js
@@ -26,7 +26,8 @@ angular.module('hakulomakkeenhallintaUiApp.services.provider')
                     formUrl: 'http://itest-virkailija.oph.ware.fi:8325/hakulomakkeenhallinta-temporary/form/:_id',
                     tarjontaAPI: 'https://itest-virkailija.oph.ware.fi/tarjonta-service/rest/v1',
                     contextRoot: '/hakulomakkeenhallinta-ui',
-                    serviceRootUri: '/hakulomakkeenhallinta-mock' //vaihetaan joskus??
+                    serviceRootUri: '/hakulomakkeenhallinta-temporary'
+//                    serviceRootUri: '/hakulomakkeenhallinta-mock'
                 };
 
             }
