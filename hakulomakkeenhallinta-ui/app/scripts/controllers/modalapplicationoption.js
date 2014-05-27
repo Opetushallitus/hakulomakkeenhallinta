c015d64832c68c1bc53d19191d4549b27463dd28
tomalatv <toni.latvakoski@gofore.com>
2014-05-27 11:13:43 +0300

    HH-167 toteutuksen aloitus

--- hakulomakkeenhallinta-ui/app/scripts/controllers/modalapplicationoption.js
+++ hakulomakkeenhallinta-ui/app/scripts/controllers/modalapplicationoption.js
@@ -9,16 +9,23 @@ angular.module('hakulomakkeenhallintaUiApp.controllers')
         $scope.applicationSystemForm = applicationSystemForm;
         $scope.userOrganizations = [];
 
-        OrganisaatioHenkilo.get().$promise.then(function(data){
+        /*OrganisaatioHenkilo.get().$promise.then(function(data){
             for(var org  in data.organisaatiot){
                 $scope.userOrganizations.push(data.organisaatiot[org].oid);
             }
             $scope.applicationOptions = HH.usersApplicationOptions($scope.userOrganizations);
-        });
+        });*/
 
         //TODO: tämä localhost hack poista!!
-/*        var orgs = ["1.2.246.562.10.82388989657", "1.2.246.562.10.65530732232" ];
-        $scope.applicationOptions = HH.usersApplicationOptions(orgs);*/
+        var orgs = ["1.2.246.562.10.82388989657", "1.2.246.562.10.65530732232" ];
+        $scope.organisations = orgs;
+
+        $scope.seclectedOrganisation = function(){
+            console.log(this.organization);
+        }
+        $scope.applicationOptions = HH.usersApplicationOptions(orgs);
+
+
 
         $scope.ok = function() {
             QuestionData.setApplicationOption(this.applicationOption);
