d3e71f02ebb5abdf3d60172d3c8aab6bca4f6f35
tomalatv <toni.latvakoski@gofore.com>
2014-05-31 18:46:36 +0300

    HH-156 hakuOid viritystä v3 typo fix

--- hakulomakkeenhallinta-ui/app/scripts/controllers/SelectHakukohde.js
+++ hakulomakkeenhallinta-ui/app/scripts/controllers/SelectHakukohde.js
@@ -5,7 +5,8 @@ angular.module('hakulomakkeenhallintaUiApp.controllers')
             $scope.applicationOptions = [];
             $scope.applicationSystemForm = HH.getApplicationSystemForm();
 
-            $scope.applicationOptions = HH.usersApplicationOptions($scope.applicationSystemForm._id, HH.getOrganization().oid);
+            $scope.applicationOptions = HH.usersApplicationOptions($scope.applicationSystemForm._id, HH.getOrganisation().oid);
+            
             console.log('####', $scope.applicationOptions);
             $scope.jatka = function(hakukohde) {
                 console.log('-- jatka -- ',hakukohde);
