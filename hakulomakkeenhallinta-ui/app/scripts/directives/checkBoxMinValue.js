047754b398986ec0f598211cf58688c5cf433a5e
tomalatv <toni.latvakoski@gofore.com>
2014-06-26 12:52:38 +0300

    HH-156 valintaruutujen min max arvojen validointi vastaamaan valintaruujen määrää

---
+++ hakulomakkeenhallinta-ui/app/scripts/directives/checkBoxMinValue.js
@@ -0,0 +1,25 @@
+'use strick';
+/**
+ * Custom lomakkeen syötekentän validaattori
+ * tarkistetaan valittavien valintaruutujen minimi määrä
+ * vastaamaan valintaruujen määrää
+ * asettaa lomakkeen $valid arvon true/false
+ */
+angular.module('hakulomakkeenhallintaUiApp.directives')
+    .directive('checkboxminvalue', function(){
+        return{
+            require: 'ngModel',
+            link: function(scope, elm, attrs, ctrl){
+                ctrl.$parsers.unshift(function(viewValue){
+                    var nroCheckBox = scope.question.options.length;
+                    if(nroCheckBox < viewValue){
+                        ctrl.$setValidity('checkboxminvalue', false);
+                        return viewValue;
+                    } else {
+                        ctrl.$setValidity('checkboxminvalue', true);
+                        return viewValue;
+                    }
+                });
+            }
+        }
+    });
