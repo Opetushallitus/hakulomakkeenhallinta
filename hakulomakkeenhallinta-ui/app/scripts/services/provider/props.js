1e8bfb09dbffa99ec3198a21079ec2c7bdbd7a38
tomalatv <toni.latvakoski@gofore.com>
2014-07-07 15:06:04 +0300

    HH logitusta muokkaa kysymystä tyyppi tiedon korjaamiseen

--- hakulomakkeenhallinta-ui/app/scripts/services/provider/props.js
+++ hakulomakkeenhallinta-ui/app/scripts/services/provider/props.js
@@ -17,7 +17,7 @@ angular.module('hakulomakkeenhallintaUiApp.services.provider')
                 };
             } else {
                 return {
-                    enableConsoleLogs: false,
+                    enableConsoleLogs: true,
                     tarjontaAPI: '/tarjonta-service/rest/v1',
                     contextRoot: '/hakulomakkeenhallinta-ui',
                     themeQuestionUri: '/haku-app/application-system-form-editor/theme-question',
