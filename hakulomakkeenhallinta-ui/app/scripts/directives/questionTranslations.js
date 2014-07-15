ed0e3b11ef62d6d3677790d836e947b7ed64e199
tomalatv <toni.latvakoski@gofore.com>
2014-07-15 16:10:32 +0300

    questionTranslations directiivin parantelua

--- hakulomakkeenhallinta-ui/app/scripts/directives/questionTranslations.js
+++ hakulomakkeenhallinta-ui/app/scripts/directives/questionTranslations.js
@@ -3,22 +3,20 @@ angular.module('hakulomakkeenhallintaUiApp.directives')
     .directive('questionTranslations', function () {
         return{
             restrict: 'E',
-                replace: true,
-                template: '<span>{{tlLanguages}}</span>',
-                link: function(scope, elem, attrs){
-                    var lngs= '';
-                    for(var lang in scope.question.messageText.translations){
-                        if(scope.question.messageText.translations['fi']){
-                            lngs +='fi ';
-                        }
-                        if(scope.question.messageText.translations['sv']){
-                            lngs +='sv ';
-                        }
-                        if(scope.question.messageText.translations['en']){
-                            lngs +='en ';
-                        }
-                    }
-                    scope.tlLanguages = lngs;
+            replace: true,
+            template: '<span>{{tlLanguages}}</span>',
+            link: function(scope, elem, attrs){
+                var lngs= '';
+                if(scope.question.messageText.translations['fi'] && lngs.indexOf('fi') === -1){
+                    lngs +='fi ';
+                }
+                if(scope.question.messageText.translations['sv'] && lngs.indexOf('sv') === -1){
+                    lngs +='sv ';
+                }
+                if(scope.question.messageText.translations['en'] && lngs.indexOf('en') === -1){
+                    lngs +='en ';
+                }
+                scope.tlLanguages = lngs;
             }
         }
     });
