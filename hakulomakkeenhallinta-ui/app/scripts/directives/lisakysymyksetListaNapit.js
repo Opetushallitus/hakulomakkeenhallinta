b9b9821f2607d2472ad9f661a1a4a71288b40c1e
tomalatv <toni.latvakoski@gofore.com>
2014-07-22 11:49:17 +0300

    HH-33 järjestä hakukohdekohtaiset lisäkysymykset v1

--- hakulomakkeenhallinta-ui/app/scripts/directives/lisakysymyksetListaNapit.js
+++ hakulomakkeenhallinta-ui/app/scripts/directives/lisakysymyksetListaNapit.js
@@ -1,9 +1,9 @@
 'use strict';
 angular.module('hakulomakkeenhallintaUiApp.directives')
-    .directive('backNappi', function () {
+    .directive('lisakysymysListaNapit', function () {
         return{
             restrict: 'E',
             replace: true,
-            templateUrl: 'partials/directives/back-nappi.html'
+            templateUrl: 'partials/directives/lisakysymys-lista-napit.html'
         }
     });
