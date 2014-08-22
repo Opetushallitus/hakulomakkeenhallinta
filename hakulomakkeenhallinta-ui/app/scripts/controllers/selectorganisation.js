70ed448a1081627b53f331f094a3e5f136d3b367
tomalatv <toni.latvakoski@gofore.com>
2014-08-22 14:55:04 +0300

    HH organisaatio valinta lista filtteröinti aakkos järjestykseen

--- hakulomakkeenhallinta-ui/app/scripts/controllers/selectorganisation.js
+++ hakulomakkeenhallinta-ui/app/scripts/controllers/selectorganisation.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('hakulomakkeenhallintaUiApp.controllers')
-    .controller('SelectOrganisationCtrl', ['$scope', '$rootScope', '$location', '$modalInstance', 'applicationSystemForm', 'Organisaatio', 'FormEditor', 'AlertMsg', 'TarjontaAPI',
-        function($scope, $rootScope, $location, $modalInstance, applicationSystemForm, Organisaatio, FormEditor, AlertMsg, TarjontaAPI ) {
+    .controller('SelectOrganisationCtrl', ['$scope', '$rootScope', '$location', '$modalInstance', 'applicationSystemForm', 'Organisaatio', 'FormEditor', 'AlertMsg', 'TarjontaAPI', '$filter',
+        function ($scope, $rootScope, $location, $modalInstance, applicationSystemForm, Organisaatio, FormEditor, AlertMsg, TarjontaAPI, $filter) {
 
             $rootScope.LOGS('SelectOrganisationCtrl');
             $scope.alerts = [];
@@ -11,30 +11,31 @@ angular.module('hakulomakkeenhallintaUiApp.controllers')
             $scope.organisations = [];
 
             $scope.$emit('LOAD');
-            $rootScope.LOGS('SelectOrganisationCtrl ','loading value:'+$scope.loading);
+            $rootScope.LOGS('SelectOrganisationCtrl ', 'loading value:' + $scope.loading);
             /**
              * haetaan valittuun hakulomakkeeseen liittyvät organisaatiot
              */
             FormEditor.getApplicationSystemFormOrgnisations(applicationSystemForm._id).then(
-                function(data){
+                function (data) {
                     $rootScope.LOGS('SelectOrganisationCtrl ', 'getApplicationSystemFormOrgnisations()', data);
                     $scope.$emit('LOADREADY');
-                    if(data.length !== 0){
-                        $scope.organisations = data;
-                    }else{
-                        if(data.status === 0){
+                    if (data.length !== 0) {
+                        $scope.organisations =  $filter('orderBy')(data, 'name.translations.' + $scope.userLang);
+                    } else {
+                        if (data.status === 0) {
                             AlertMsg($scope, 'warning', 'organisaatioita.ei.saatu.ladattua');
                         }
                         AlertMsg($scope, 'warning', 'ei.riittavia.kaytto.oikeuksia.tahan.hakuun');
                     }
-            });
+                }
+            );
             /**
              * asettaa valitun organisaation organisaation serviceen talteen myöhempää
              * käyttöä varten
              */
-            $scope.selectedOrganisation = function(){
-                if(this.organisation !==null){
-                    $rootScope.LOGS('SelectOrganisationCtrl', 'selectedOrganisation()' , this.organisation);
+            $scope.selectedOrganisation = function () {
+                if (this.organisation !==null) {
+                    $rootScope.LOGS('SelectOrganisationCtrl', 'selectedOrganisation()', this.organisation);
                     Organisaatio.setOrganisation(this.organisation);
                     $scope.applicationOptions = TarjontaAPI.usersApplicationOptions(applicationSystemForm._id, this.organisation.id);
                 }
@@ -42,18 +43,16 @@ angular.module('hakulomakkeenhallintaUiApp.controllers')
             /**
              * organisaation valinnan jälkeen siirrytään hakemuksen lisäkysymys sivulle
              */
-            $scope.jatka = function() {
+            $scope.jatka = function () {
                 $rootScope.LOGS('SelectOrganisationCtrl ', 'jatka() ', Organisaatio.getOrganisation().id);
-                if(Organisaatio.getOrganisation().id !== undefined){
+                if (Organisaatio.getOrganisation().id !== undefined) {
                     FormEditor.setApplicationSystemForm(applicationSystemForm);
                     $modalInstance.dismiss('ok');
-                    $location.path("/themeQuestionsByOrganisation/" + $scope.applicationSystemForm._id+'/'+Organisaatio.getOrganisation().id );
+                    $location.path("/themeQuestionsByOrganisation/" + $scope.applicationSystemForm._id + '/' + Organisaatio.getOrganisation().id);
                 }
             };
 
-            $scope.cancel = function() {
+            $scope.cancel = function () {
                 $modalInstance.dismiss('cancel');
             };
-
-
         }]);
