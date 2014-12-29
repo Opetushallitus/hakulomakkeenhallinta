ddcc1f1cf6232b3ae57c65b263c03e4b664a9276
tomalatv <toni.latvakoski@gofore.com>
2014-12-29 16:33:36 +0200

    HH-19 hakukohderyhmän ensisijaisuustoiminnallisuus prioriteetti dialogilla + mock prioriteetit hakukohteille v2

--- hakulomakkeenhallinta-ui/app/scripts/controllers/prioriteettienAsettaminenDialogCtrl.js
+++ hakulomakkeenhallinta-ui/app/scripts/controllers/prioriteettienAsettaminenDialogCtrl.js
@@ -1,11 +1,35 @@
 angular.module('hakulomakkeenhallintaUiApp.controllers')
-    .controller('prioriteettienAsettaminenDialogCtrl', function ($rootScope, $scope, $modalInstance, hakukohteet) {
+    .controller('prioriteettienAsettaminenDialogCtrl', function ($rootScope, $scope, $modalInstance, hakukohteet, _) {
 
+        $scope.hakukohteet = hakukohteet;
 
         $scope.tallennaPrioriteetit = function () {
           console.log('Tähän prioriteettien tallennusta');
         };
-
+        /**
+         * siirretaan hakukohteen prioriteettiä ylemmälle
+         * prioriteetille
+         * @param hakukohde
+         */
+        $scope.movePriorityUp = function (hakukohde) {
+            console.log('UP', hakukohde.prioriteetti);
+            var prioriteetti = hakukohde.prioriteetti;
+            $scope.hakukohteet[prioriteetti] = _.without($scope.hakukohteet[prioriteetti], hakukohde);
+            hakukohde.prioriteetti = hakukohde.prioriteetti + 1;
+            $scope.hakukohteet[prioriteetti + 1].push(hakukohde);
+        };
+        /**
+         * siirretään hakukohteen prioriteettia alemmalle
+         * prioriteetille
+         * @param hakukohde
+         */
+        $scope.movePriorityDown = function (hakukohde) {
+            console.log('DOWN', hakukohde.prioriteetti);
+            var prioriteetti = hakukohde.prioriteetti;
+            $scope.hakukohteet[prioriteetti] = _.without($scope.hakukohteet[prioriteetti], hakukohde);
+            hakukohde.prioriteetti = hakukohde.prioriteetti - 1;
+            $scope.hakukohteet[prioriteetti - 1].push(hakukohde);
+        };
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
         };
