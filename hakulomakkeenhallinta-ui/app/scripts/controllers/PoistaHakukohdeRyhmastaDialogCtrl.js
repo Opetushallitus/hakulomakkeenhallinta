b3b40c1b540d3cbb2d963882590bcac931a46be3
tomalatv <toni.latvakoski@gofore.com>
2015-01-20 11:01:01 +0200

    HH-19 ja HH-175 hakukohteen poisto ryhmästä tarjonnan autentikoinnin tarkastus ennen poistoa

--- hakulomakkeenhallinta-ui/app/scripts/controllers/PoistaHakukohdeRyhmastaDialogCtrl.js
+++ hakulomakkeenhallinta-ui/app/scripts/controllers/PoistaHakukohdeRyhmastaDialogCtrl.js
@@ -1,10 +1,27 @@
 'use strict';
 
 angular.module('hakulomakkeenhallintaUiApp.controllers')
-    .controller('PoistaHakukohdeRyhmastaDialogCtrl', function ($rootScope, $scope, $modalInstance, hakukohdeRyhma, hakukohde, TarjontaAPI, AlertMsg) {
+    .controller('PoistaHakukohdeRyhmastaDialogCtrl',
+    function ($rootScope, $scope, $modalInstance, hakukohdeRyhma, hakukohde, TarjontaAPI, AlertMsg, LocalisationService) {
         $scope.hakukohde = hakukohde;
         $scope.hakukohdeRyhma = hakukohdeRyhma;
         $scope.poistoEiOnnistu = false;
+        TarjontaAPI.checkTarjontaAuthentication().then(
+            function success (data) {
+                //TODO: poista logitukset kun ei tarvetta
+                console.log('***** TARKISTETAAN AUTENTIKAATIO: ', data);
+            },
+            function error (resp) {
+                //TODO: poista logitukset kun ei tarvetta
+                console.log('***** EI OIKEUTTA ', resp);
+                $scope.poistoEiOnnistu = true;
+                AlertMsg($scope, 'warning', 'warning.autenkikaatio.ei.onnistunut.tai.puutuvat.oikeudet.tarjonta.palvelu');
+            }
+        );
+        /**
+         * Poistetaan hakukohde hakukohderyhmästä
+         * tiedon tallennuspaikka tarjonta palvelu
+         */
         $scope.poista = function () {
             TarjontaAPI.poistaHakukohdeRyhmasta(hakukohdeRyhma, hakukohde).then(
                 function success() {
@@ -21,7 +38,11 @@ angular.module('hakulomakkeenhallintaUiApp.controllers')
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
         };
-    });
+        $scope.t = function (key) {
+            return LocalisationService.tl(key);
+        };
+    }
+);
 
 
 
--- hakulomakkeenhallinta-ui/app/scripts/directives/priorisoivaHakukohdeRyhmaInfo.js
+++ hakulomakkeenhallinta-ui/app/scripts/directives/priorisoivaHakukohdeRyhmaInfo.js
@@ -2,80 +2,94 @@
 
 angular.module('hakulomakkeenhallintaUiApp.directives')
     .directive('priorisoivaHakukohdeRyhmaInfo',
-        function (TarjontaAPI, _, $modal, AlertMsg) {
-            return {
-                restrict: 'E',
-                replace: true,
-                templateUrl: 'partials/directives/priorisoiva-hakukohde-ryhma-info.html',
-                scope: {
-                    priorisointiRyhma: '=priorisointiRyhma'
-                },
-                controller: function ($scope) {
-                    $scope.naytaHakukohdeLista = false;
-                    $scope.hakukohteidenMaara = 0;
-                    //$scope.priorisoivaHakukohdeRyhma = {};
-                    /**
-                     * haetaan ryhmän hakukohteet
-                     */
-                    TarjontaAPI.haeRyhmanHakukohteet($scope.priorisointiRyhma.oid).then(
-                        function (data) {
-                            $scope.hakukohteet = _.flatten(
-                                _.map(data.tulokset, function(tulokset) {
-                                        //TODO: muuta tämä kun oikeat prioriteetit saatavilla tarjonnasta
-                                        var pri  = 1;
-                                        return _.each(tulokset.tulokset, function (tulos) {
-                                                tulos.prioriteetti = pri;
-                                                tulos.tarjoaja = {};
-                                                tulos.tarjoaja.nimi = tulokset.nimi;
-                                                pri += 1;
-                                                if (pri === 5) {
-                                                    pri = 1;
-                                                    delete tulos.prioriteetti;
-                                                }
-                                                return tulos;
+    function (TarjontaAPI, _, $modal, AlertMsg, TarjontaService) {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'partials/directives/priorisoiva-hakukohde-ryhma-info.html',
+            scope: {
+                priorisointiRyhma: '=priorisointiRyhma',
+                userLang: '@userLang'
+            },
+            controller: function ($scope) {
+                $scope.naytaHakukohdeLista = false;
+                $scope.hakukohteidenMaara = 0;
+                /**
+                 * haetaan ryhmän hakukohteet
+                 */
+                TarjontaAPI.haeRyhmanHakukohteet($scope.priorisointiRyhma.oid).then(
+                    function (data) {
+                        $scope.hakukohteet = _.flatten(
+                            _.map(data.tulokset, function(tulokset) {
+                                    //TODO: muuta tämä kun oikeat prioriteetit saatavilla tarjonnasta
+                                    var pri  = 1;
+                                    return _.each(tulokset.tulokset, function (tulos) {
+                                            tulos.prioriteetti = pri;
+                                            tulos.tarjoaja = {};
+                                            tulos.tarjoaja.nimi = tulokset.nimi;
+                                            pri += 1;
+                                            if (pri === 5) {
+                                                pri = 1;
+                                                delete tulos.prioriteetti;
                                             }
-                                        );
-                                    }
-                                )
-                            );
-                            var prioriteetit, prioriteettiRyhmat = {};
-                            prioriteetit = _.uniq(_.map($scope.hakukohteet, function(hakukohde) { return hakukohde.prioriteetti; }));
-                            _.each(prioriteetit, function (prioriteetti) {
-                                    prioriteettiRyhmat[prioriteetti] =
-                                        _.where($scope.hakukohteet, {prioriteetti: prioriteetti});
+                                            return tulos;
+                                        }
+                                    );
                                 }
-                            );
-                            $scope.hakukohteet = prioriteettiRyhmat;
-                            $scope.hakukohteidenMaara = data.tuloksia;
-                        }
-                    );
-
-                    $scope.toggleNaytaHakukohteet = function () {
-                        $scope.naytaHakukohdeLista = !$scope.naytaHakukohdeLista;
-                    };
-                    /**
-                     * Avataan dialogi priorisointien muuttamiseksi
-                     */
-                    $scope.muokkaaPrioriteetteja = function () {
-                        $modal.open({
-                            templateUrl: 'partials/dialogs/prioriteettien-asettaminen-dialog.html',
-                            controller: 'prioriteettienAsettaminenDialogCtrl',
-                            scope: $scope,
-                            size: 'lg',
-                            resolve: {
-                                hakukohteet: function (){
-                                    return $scope.hakukohteet;
-                                }
-                            }
-                        }).result.then(
-                            function(data) {
-                                $scope.hakukohteet = data;
+                            )
+                        );
+                        var prioriteetit, prioriteettiRyhmat = {};
+                        prioriteetit = _.uniq(_.map($scope.hakukohteet, function(hakukohde) { return hakukohde.prioriteetti; }));
+                        _.each(prioriteetit, function (prioriteetti) {
+                                prioriteettiRyhmat[prioriteetti] =
+                                    _.where($scope.hakukohteet, {prioriteetti: prioriteetti});
                             }
                         );
+                        $scope.hakukohteet = prioriteettiRyhmat;
+                        $scope.hakukohteidenMaara = data.tuloksia;
                     }
+                );
 
-                }
-            };
+                $scope.toggleNaytaHakukohteet = function () {
+                    $scope.naytaHakukohdeLista = !$scope.naytaHakukohdeLista;
+                };
+                /**
+                 * Avataan dialogi priorisointien muuttamiseksi
+                 */
+                $scope.muokkaaPrioriteetteja = function () {
+                    $modal.open({
+                        templateUrl: 'partials/dialogs/prioriteettien-asettaminen-dialog.html',
+                        controller: 'prioriteettienAsettaminenDialogCtrl',
+                        scope: $scope,
+                        size: 'lg',
+                        resolve: {
+                            hakukohteet: function (){
+                                return $scope.hakukohteet;
+                            }
+                        }
+                    }).result.then(
+                        function(data) {
+                            $scope.hakukohteet = data;
+                        }
+                    );
+                };
+                /**
+                 * avataan dialogi lisätään hakukohde ryhmään
+                 * @param hakukohdeRyhma
+                 */
+                $scope.lisaaHakukohdeRyhmaan = function (hakukohdeRyhma) {
+                    TarjontaService.lisaaHakukohdeRyhmaan(hakukohdeRyhma, $scope.userLang);
+                };
+                /**
+                 * avataan dialogi poista hakukohde ryhmästä
+                 * @param hakukohdeRyhma
+                 * @param hakukohde
+                 */
+                $scope.poistaHakukohdeRyhmasta = function (hakukohdeRyhma, hakukohde) {
+                    TarjontaService.poistaHakukohdeRyhmasta(hakukohdeRyhma, hakukohde);
+                };
 
-        }
-    );
+            }
+        };
+    }
+);
