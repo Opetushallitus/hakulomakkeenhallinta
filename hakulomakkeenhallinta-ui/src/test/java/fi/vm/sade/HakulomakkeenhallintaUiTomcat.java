d94884406d7d5a6d6bc8eb1b03df2793660460d7
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-02-26 09:38:14 +0200

    add embedded runner for tests and vagrant

---
+++ hakulomakkeenhallinta-ui/src/test/java/fi/vm/sade/HakulomakkeenhallintaUiTomcat.java
@@ -0,0 +1,46 @@
+package fi.vm.sade;
+
+import fi.vm.sade.integrationtest.tomcat.EmbeddedTomcat;
+import fi.vm.sade.integrationtest.tomcat.SharedTomcat;
+import fi.vm.sade.integrationtest.util.PortChecker;
+import fi.vm.sade.integrationtest.util.ProjectRootFinder;
+import org.apache.catalina.LifecycleException;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+
+public class HakulomakkeenhallintaUiTomcat extends EmbeddedTomcat {
+    static final String MODULE_ROOT = ProjectRootFinder.findProjectRoot() + "/hakulomakkeenhallinta-ui";
+    static final String CONTEXT_PATH = "/hakulomakkeenhallinta-ui";
+    static final int DEFAULT_PORT = 9092;
+
+    public final static void main(String... args) throws ServletException, LifecycleException {
+        useIntegrationTestSettingsIfNoProfileSelected();
+        new HakulomakkeenhallintaUiTomcat(Integer.parseInt(System.getProperty("/hakulomakkeenhallinta-ui-app.port", String.valueOf(DEFAULT_PORT)))).start().await();
+    }
+
+    public HakulomakkeenhallintaUiTomcat(int port) {
+        super(port, MODULE_ROOT, CONTEXT_PATH);
+    }
+
+    public static void startShared() {
+        SharedTomcat.start(MODULE_ROOT, CONTEXT_PATH);
+    }
+
+    public static void startForIntegrationTestIfNotRunning() {
+        useIntegrationTestSettingsIfNoProfileSelected();
+        if (PortChecker.isFreeLocalPort(DEFAULT_PORT)) {
+            new HakulomakkeenhallintaUiTomcat(DEFAULT_PORT).start();
+        } else {
+            LoggerFactory.getLogger(HakulomakkeenhallintaUiTomcat.class).info("Not starting Tomcat: seems to be running on port " + DEFAULT_PORT);
+        }
+    }
+
+    private static void useIntegrationTestSettingsIfNoProfileSelected() {
+        System.setProperty("application.system.cache", "false");
+        if (System.getProperty("spring.profiles.active") == null) {
+            System.setProperty("spring.profiles.active", "it");
+        }
+        System.out.println("Running embedded with profile " + System.getProperty("spring.profiles.active"));
+    }
+}
