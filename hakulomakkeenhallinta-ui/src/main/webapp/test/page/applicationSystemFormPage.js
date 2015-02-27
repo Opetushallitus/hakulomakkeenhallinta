84aa603e9b43a6c496494367aaeee2120e95a2f8
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-02-27 13:08:57 +0200

    fix test setup and assert

--- hakulomakkeenhallinta-ui/src/main/webapp/test/page/applicationSystemFormPage.js
+++ hakulomakkeenhallinta-ui/src/main/webapp/test/page/applicationSystemFormPage.js
@@ -18,7 +18,38 @@ function ApplicationSystemFormPage() {
 
     selectNthFromDropdown: function(row, nth) { row.find("li:nth(" + nth + ") a")[0].click() },
 
-    loaded: function() {
+    openAddRestrictionPopup: function() {
+      return function() {
+        var deferred = Q.defer()
+        var formRow = api.applicationFormRowByName("Testihaku testipisteeseen")
+        api.openDropdown(formRow)
+        api.selectNthFromDropdown(formRow, 1) // Lomakepohjan asetukset
+
+        var settingsRow = []
+        wait.until(function() {
+          settingsRow = api.applicationFormSettingsRowByName("Rajaavat hakukohderyhmät")
+          return settingsRow.length == 1
+        })().then(function() {
+          api.openRow(settingsRow)
+          var limitingRow = []
+          wait.until(function() {
+            limitingRow = api.applicationFormSettingsRowByName("Vaasan yliopisto, maisterihaku, hallintotieteet")
+            return limitingRow.length == 1
+          })().then(function() {
+            api.openDropdown(limitingRow)
+            api.selectNthFromDropdown(limitingRow, 0) // Aseta rajaus
+            wait.until(function() {
+              return S('h1.ng-binding').toArray().length > 1
+            })().then(function() {
+              deferred.resolve()
+            })
+          })
+        })
+        return deferred.promise
+      }
+  },
+
+  loaded: function() {
       return S("td.ng-binding").toArray().length > 0
     }
   }
--- hakulomakkeenhallinta-ui/src/main/webapp/test/spec/applicationSystemFormSpec.js
+++ hakulomakkeenhallinta-ui/src/main/webapp/test/spec/applicationSystemFormSpec.js
@@ -13,28 +13,10 @@
     })
 
     describe("rajaavan hakukohderyhmän lisääminen", function() {
-      it('Testihaku testipisteeseen rajaavan hakukohderyhmän lisäys', function () {
-        var formRow = page.applicationFormRowByName("Testihaku testipisteeseen")
-        page.openDropdown(formRow)
-        page.selectNthFromDropdown(formRow, 1) // Lomakepohjan asetukset
+      before(page.openAddRestrictionPopup())
 
-        var settingsRow = []
-        wait.until(function() {
-          settingsRow = page.applicationFormSettingsRowByName("Rajaavat hakukohderyhmät")
-          return settingsRow.length == 1
-        })().then(function() {
-          page.openRow(settingsRow)
-          var limitingRow = []
-          wait.until(function() {
-            limitingRow = page.applicationFormSettingsRowByName("Vaasan yliopisto, maisterihaku, hallintotieteet")
-            return limitingRow.length == 1
-          })().then(function() {
-            page.openDropdown(limitingRow)
-            page.selectNthFromDropdown(formRow, 1) // Aseta rajaus
-          })
-
-        })
-
+      it('Testihaku testipisteeseen rajaavan hakukohderyhmän lisäys', function() {
+        expect(S('h1.ng-binding:nth(1)').text().trim()).to.equal("Testihaku testipisteeseen")
       })
     })
   })
