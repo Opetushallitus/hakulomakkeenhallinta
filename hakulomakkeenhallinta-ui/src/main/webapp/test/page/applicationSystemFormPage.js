d23507a5ec3a1bad351bc94dc5f6d8e0b9da6bb5
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-02-27 09:14:00 +0200

    add second test skeleton

--- hakulomakkeenhallinta-ui/src/main/webapp/test/page/applicationSystemFormPage.js
+++ hakulomakkeenhallinta-ui/src/main/webapp/test/page/applicationSystemFormPage.js
@@ -7,6 +7,11 @@ function ApplicationSystemFormPage() {
     applicationFormRowByName: function(name) {
       return $(_.find(S("td.ng-binding"), function(e) { return $(e).text() == name })).parent()
     },
+
+    applicationFormSettingsRowByName: function(name) {
+      return $(_.find(S("a.ng-binding"), function(e) { console.log($(e).text().trim()); return $(e).text().trim() == name })).parent().parent()
+    },
+
     loaded: function() {
       return S("td.ng-binding").toArray().length > 0
     }
--- hakulomakkeenhallinta-ui/src/main/webapp/test/spec/applicationSystemFormSpec.js
+++ hakulomakkeenhallinta-ui/src/main/webapp/test/spec/applicationSystemFormSpec.js
@@ -10,7 +10,32 @@
       it('Testihaku testipisteeseen löytyy lomakkeelta', function () {
         expect(page.applicationFormRowByName("Testihaku testipisteeseen").length).to.equal(1)
       })
-
     })
+
+    describe("rajaavan hakukohderyhmän lisääminen", function() {
+      it('Testihaku testipisteeseen rajaavan hakukohderyhmän lisäys', function () {
+        var formRow = page.applicationFormRowByName("Testihaku testipisteeseen")
+        openDropdown(formRow)
+        selectNthFromDropdown(formRow, 1) // Lomakepohjan asetukset
+
+        var settingsRow = []
+        wait.until(function() {
+          settingsRow = page.applicationFormSettingsRowByName("Rajaavat hakukohderyhmät")
+          return settingsRow.length == 1
+        })().then(function() {
+          openDropdown(settingsRow)
+          selectNthFromDropdown(settingsRow, 0) // Lisää ryhmä asetuksiin
+        })
+
+      })
+    })
+
+    function openDropdown(row) {
+      row.find("i.hh-icon-menu").click()
+    }
+
+    function selectNthFromDropdown(row, nth) {
+      row.find("li:nth(" + nth + ") a")[0].click()
+    }
   })
 })()
