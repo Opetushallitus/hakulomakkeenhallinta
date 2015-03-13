83c39f78cb5aa5aa16d52bcd50deade4d7166d33
Eetu Blomqvist <eetu.blomqvist@reaktor.fi>
2015-03-13 13:05:27 +0200

    Respect checkbox value for highest ao address when reading from db

--- hakulomakkeenhallinta-ui/src/main/webapp/app/scripts/directives/hakukohdeRyhmaInfo.js
+++ hakulomakkeenhallinta-ui/src/main/webapp/app/scripts/directives/hakukohdeRyhmaInfo.js
@@ -26,9 +26,7 @@ angular.module('hakulomakkeenhallintaUiApp.directives')
                     deliveryDue: undefined
                 };
                 if($scope.ryhma.configurations) {
-                    if($scope.ryhma.configurations.deadline) {
-                        $scope.hakukohderyhmanOsoite.useFirstAoAddress = ($scope.ryhma.configurations.useFirstAoAddress === 'true');
-                    }
+                    $scope.hakukohderyhmanOsoite.useFirstAoAddress = ($scope.ryhma.configurations.useFirstAoAddress === 'true');
                     $scope.hakukohderyhmanOsoite.address.recipient = $scope.ryhma.configurations.addressRecipient;
                     $scope.hakukohderyhmanOsoite.address.street = $scope.ryhma.configurations.addressStreet;
                     $scope.hakukohderyhmanOsoite.address.postCode = $scope.ryhma.configurations.addressPostalCode;
