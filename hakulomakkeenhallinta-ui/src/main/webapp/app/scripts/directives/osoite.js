01617724e21270ff3688e2cd58418efef2d63e23
Eetu Blomqvist <eetu.blomqvist@reaktor.fi>
2015-03-13 14:05:14 +0200

    Better validation for address

--- hakulomakkeenhallinta-ui/src/main/webapp/app/scripts/directives/osoite.js
+++ hakulomakkeenhallinta-ui/src/main/webapp/app/scripts/directives/osoite.js
@@ -9,7 +9,10 @@ angular.module('hakulomakkeenhallintaUiApp.directives')
             templateUrl: 'partials/directives/osoite.html',
             scope: {
                 address: '=address',
-                userLang: '@userLang'
+                userLang: '@userLang',
+                disabled: "=",
+                requireRecipient: "@",
+                noPrefill: "@"
             },
             controller: function ($scope) {
                 $scope.t = function (key) {
@@ -37,21 +40,23 @@ angular.module('hakulomakkeenhallintaUiApp.directives')
                  * Esitätäytetään liitepyynnön palautus osoitteeksi käyttäjän organisaatiosta saatu postiosoite
                  * jos se on saatavilla.
                  */
-                Organisaatio.getOrganisationData($routeParams.oid).then(
-                    function (orgInfo) {
-                        if (orgInfo.postiosoite) {
-                            if (orgInfo.postiosoite.osoite && $scope.address.street === undefined) {
-                                $scope.address.street = orgInfo.postiosoite.osoite;
-                            }
-                            if (orgInfo.postiosoite.postinumeroUri && $scope.address.postCode === undefined) {
-                                $scope.address.postCode = orgInfo.postiosoite.postinumeroUri.slice(6);
-                            }
-                            if (orgInfo.postiosoite.postitoimipaikka && $scope.address.postOffice === undefined) {
-                                $scope.address.postOffice = orgInfo.postiosoite.postitoimipaikka;
+                if(!$scope.noPrefill) {
+                    Organisaatio.getOrganisationData($routeParams.oid).then(
+                        function (orgInfo) {
+                            if (orgInfo.postiosoite) {
+                                if (orgInfo.postiosoite.osoite && $scope.address.street === undefined) {
+                                    $scope.address.street = orgInfo.postiosoite.osoite;
+                                }
+                                if (orgInfo.postiosoite.postinumeroUri && $scope.address.postCode === undefined) {
+                                    $scope.address.postCode = orgInfo.postiosoite.postinumeroUri.slice(6);
+                                }
+                                if (orgInfo.postiosoite.postitoimipaikka && $scope.address.postOffice === undefined) {
+                                    $scope.address.postOffice = orgInfo.postiosoite.postitoimipaikka;
+                                }
                             }
                         }
-                    }
-                );
+                    );
+                }
             }
         };
 
